4013b77fcc938083daf841e2cb50baf15d6124ac
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2019-01-21 16:37:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,51 +34,45 @@ client.user.setGame(`!!help | !!inv`,"https://www.twitch.tv/dggamingbot")
 
 
 
-client.on('ready', function(){
- console.log(`Logged in as ${client.user.tag}!`); 
-   
-   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293']; // صور اضافيه
-   var s = ['483055660209012736','480169573530861578','483055655800930315'];  // صور  الي بتشغل
-    setInterval(function (){
-    client.user.setPresence({
- game: { 
-    type: 1,
-     url: 'https://www.twitch.tv/n3k4a',
-    name: 'Alone ,',  // الكلام الي في التوتش
-    application_id: '512333311582928916', // ايدي البوت او ايدي الحساب حقك 
-     assets: {
-         large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
- 
-    }
-  }
-    });
-    }, 5000);//سرعه تغير الصور
-});
+client.on('message', message => { 
+    if(!message.channel.guild) return;
+ if(message.content.startsWith(prefix + 'clear')) {
+ if(!message.channel.guild) return message.channel.send('**هذا الامر فقط للسيرفرات**').then(m => m.delete(5000));
+ if(!message.member.hasPermission('MANAGE_MESSAGE')) return      message.channel.send('**ليس لديك برمشن manage message`' );
+ let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+ let request = `Requested By ${message.author.username}`;
+ message.channel.send(`**هل انت متأكد من حذف الشات؟**`).then(msg => {
+ msg.react('✅')
+ .then(() => msg.react('❌'))
+ .then(() =>msg.react('✅'))
 
+ let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+ let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
 
+ let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+ let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+ reaction1.on("collect", r => {
+ message.channel.send(`سينحذف الشات ...`).then(m => m.delete(5000));
+ var msg;
+         msg = parseInt();
 
-client.on("message", message => {
-    var prefix = "!!";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم",
-        color: 0x00a300,
-        description: ":white_check_mark: | Delete " + args[1] + " Message!",
-        footer: {
-          text: "Slash Bot"
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
+       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+       message.channel.sendMessage("", {embed: {
+         title: "`` تــــم حذف الشات ``",
+         color: 0x06DF00,
+         footer: {
 
-     
-});
+         }
+       }}).then(msg => {msg.delete(3000)});
+
+ })
+ reaction2.on("collect", r => {
+ message.channel.send(`**تم الغاء حذف الشات**`).then(m => m.delete(5000));
+ msg.delete();
+ })
+ })
+ }
+ });
 
 
 
