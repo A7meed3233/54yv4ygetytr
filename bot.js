2d03312cdc14b0342f31752f0e93f15be8994393
A7meed3233 <43696377+A7meed3233@users.noreply.github.com>
2019-01-30 11:56:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const prefix = '!!'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`!!help | !!inv`,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(`80 %`,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -1419,63 +1419,26 @@ client.on('message', message => {
   }
 });
 
-
-
-
-const sug = JSON.parse(fs.readFileSync('./sug.json' , 'utf8'));
- 
 client.on('message', message => {
-           if (!message.channel.guild) return;
- 
-    let room = message.content.split(" ").slice(1);
-    let findroom = message.guild.channels.find('name', `${room}`)
-    if(message.content.startsWith(prefix + "setSug")) {
-        if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
-        if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
-if(!room) return message.channel.send('Please Type The Channel Name')
-if(!findroom) return message.channel.send('Cant Find This Channel')
-let embed = new Discord.RichEmbed()
-.setTitle('**Done The Suggest Code Has Been Setup**')
-.addField('Channel:', `${room}`)
-.addField('Requested By:', `${message.author}`)
-.setThumbnail(message.author.avatarURL)
-.setFooter(`${client.user.username}`)
-message.channel.sendEmbed(embed)
-sug[message.guild.id] = {
-channel: room,
-}
-fs.writeFile("./sug.json", JSON.stringify(sug), (err) => {
-if (err) console.error(err)
-})
-   client.on('message', message => {
- 
- 
-    if(message.content.startsWith(`${prefix}suggest`)) {
-      if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
-      let suggest = message.content.split(" ").slice(1);
-      if(!suggest) return message.reply(`**Please Type The Suggest**`)
-    let findchannel = (message.guild.channels.find('name', `${sug[message.guild.id].channel}`))
-    if(!findchannel) return message.channel.send(`Error 404: The Suggest Channel Cant Find Or Not Set To Set The Suggest Channel Type: ${prefix}setSug`)
-    message.channel.send(`Done Your Suggest Will Be Seen By The Staffs`)
-    let sugembed = new Discord.RichEmbed()
-    .setTitle('New Suggest !')
-    .addField('Suggest By:', `${message.author}`)
-    .addField('Suggest:', `${suggest}`)
-    .setFooter(client.user.username)
-    findchannel.sendEmbed(sugembed)
-        .then(function (message) {
-          message.react('✅')
-          message.react('❌')
-        })
-        .catch(err => {
-            message.reply(`Error 404: The Suggest Channel Cant Find Or Not Set To Set The Suggest Channel Type: ${prefix}setSug`)
-            console.error(err);
-        });
-        }
-      })
-    }})
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
 
-
+  if(!message.content.toLowerCase().startsWith(prefix)) return;
+  if(command == "suggest") {
+    if(!args.join(" ")) return message.channel.send(`**يرجي كتابة الاقتراح **`);
+    let channel = message.guild.channels.find(c => c.name == "suggestions");
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setTitle(``)
+    .setFooter(`Select a reaction below to vote on suggestion`)
+    .setDescription(args.join(" "));
+    channel.send(embed).then(msg => {
+      msg.react("✅").then(() => msg.react("❌"));
+      message.delete()
+      message.channel.send(`**يرجي كتابة اقتراح لكي يتم ارساله الي روم الاقتراحات ❎ **`);
+    });
+  }
+});
 
 const reportjson = JSON.parse(fs.readFileSync('./report.json' , 'utf8'));
  
